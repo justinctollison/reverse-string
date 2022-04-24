28f0a0b2beee8e891e7d797c309109e90220e8cc
justinctollison <justinctollison@gmail.com>
2022-04-24 13:32:51 -0700

    Completed assignment

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,21 @@
 function reverseString(str) {
   // type your code here
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]
+  }
+  
+   return reversed;
+
 }
 
 if (require.main === module) {
   // add your own tests in here
+
+  console.log("Expecting 654321")
+  console.log("=>", reverseString("123456"))
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -17,3 +29,7 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+  //set reversed variable to empty array
+  //iterate over the str variable, over each character in the word
+  //return the order of the characters in reverse, from latest array position to nearest array position --> 01234 -> 43210
